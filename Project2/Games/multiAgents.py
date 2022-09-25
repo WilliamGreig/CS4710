0a84829ca2b8f96b2466655fa719197e077645a9
William Greig <wpg6zmk@virginia.edu>
2022-09-25 18:23:45 -0400

    finished project haha

--- Project2/Games/multiAgents.py
+++ Project2/Games/multiAgents.py
@@ -215,8 +215,60 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
         Returns the minimax action using self.depth and self.evaluationFunction
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # okay alpha_beta is basically minimax but you stop looking at branches/trees if they are too small/large depending on what has already been found for the above nodes
+
+        action = self.alpha_beta(gameState)
+        return action
+
+
+    def alpha_beta(self, state):
+        # similar to minimax but now include alpha beta
+        pacman = 0
+        actions = state.getLegalActions(pacman)
+        best_act = None
+        depth = 0
+        next_agent = (pacman + 1) % state.getNumAgents()
+        alpha = float('-inf')
+        beta = float('inf')
+        for a in actions:
+            v = self.min_value(state.generateSuccessor(pacman, a), next_agent, depth, alpha, beta)
+            if v > alpha:
+                alpha = v
+                best_act = a
+        return best_act
+
+    def max_value(self, state, agentIndex, depth, alpha, beta):
+        if depth == self.depth or state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        actions = state.getLegalActions(agentIndex)
+        next_agent = (agentIndex + 1) % state.getNumAgents()
+        v = float('-inf')
+        for a in actions:
+            v = max(v, self.min_value(state.generateSuccessor(agentIndex, a), next_agent, depth, alpha, beta))
+            if v > beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+
+    def min_value(self, state, agentIndex, depth, alpha, beta):
+        if depth == self.depth or state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        actions = state.getLegalActions(agentIndex)
+        v = float('inf')
+        next_agent = (agentIndex + 1) % state.getNumAgents()
+        for a in actions:
+            # if the next agent is not pacman, don't increment depth and get minimum value (adversary)
+            if next_agent != 0:
+                v = min(v, self.min_value(state.generateSuccessor(agentIndex, a), next_agent, depth, alpha, beta))
+            else:
+                # otherwise, it is pacman and use max value
+                v = min(v, self.max_value(state.generateSuccessor(agentIndex, a), next_agent, depth + 1, alpha, beta))
+            # i'm not sure why v <= alpha doesn't work...
+            if v < alpha:
+                return v
+            beta = min(beta, v)
+        return v
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -230,18 +282,101 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         All ghosts should be modeled as choosing uniformly at random from their
         legal moves.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        action = self.expectimax(gameState)
+        return action
+
+
+    def expectimax(self, state):
+        # get the max value action from the minimum successor
+        pacman = 0
+        actions = state.getLegalActions(pacman)
+        v = float('-inf')
+        best_act = None
+        depth = 0
+        next_agent = (pacman + 1) % state.getNumAgents()
+        for a in actions:
+            score = self.min_value(state.generateSuccessor(pacman, a), next_agent, depth)
+            if score > v:
+                v = score
+                best_act = a
+        return best_act
+
+    def max_value(self, state, agentIndex, depth):
+        if depth == self.depth or state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        actions = state.getLegalActions(agentIndex)
+        next_agent = (agentIndex + 1) % state.getNumAgents()
+        v = float('-inf')
+        for a in actions:
+            v = max(v, self.min_value(state.generateSuccessor(agentIndex, a), next_agent, depth))
+        return v
+
+
+    def min_value(self, state, agentIndex, depth):
+        if depth == self.depth or state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        actions = state.getLegalActions(agentIndex)
+        v = 0
+        next_agent = (agentIndex + 1) % state.getNumAgents()
+        # the probability is uniform -- so all actions are the same (ie) -- 1/num. of actions
+        probability = 1/len(actions)
+        for a in actions:
+            # if the next agent is not pacman, don't increment depth and get minimum value (adversary)
+            if next_agent != 0:
+                v = v + self.min_value(state.generateSuccessor(agentIndex, a), next_agent, depth) * probability
+            else:
+                # otherwise, it is pacman and use max value
+                v = v + self.max_value(state.generateSuccessor(agentIndex, a), next_agent, depth + 1) * probability
+        return v
 
 def betterEvaluationFunction(currentGameState):
     """
     Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
     evaluation function (question 5).
 
-    DESCRIPTION: <write something here so we know what you did>
+    DESCRIPTION: My mega-pacman, who I will call Jerry, will be scored using a few features (actually, this is the same evaluation function that I used above but it works, so okay!):
+        + current score -- this is a plus for Jerry because the higher the score, the better he is doing (also, we want to make him move faster and higher score means less time wasted)
+        + near_food -- we are adding the inverse of manhattan distance between Jerry and the nearest food; the idea is that the closer he is to food, the better Jerry is performing (as he can eat it!)
+        - number of remaining food -- we subtract the amount of food still remaining on the board as an incentive to go and eat more food; I found that dividing it by the number of tiles (total squares on the grid) was most effective in getting a better score
+        - ghost distance -- we subtract the inverse of the manhattan distance between Jerry and the nearest ghost; if ghosts are far, this is good for Jerry. if ghosts are near, he needs to be careful and therefore his score is lower
     """
+    successorGameState = currentGameState
+    newPos = successorGameState.getPacmanPosition()
+    newFood = successorGameState.getFood()
+    newGhostStates = successorGameState.getGhostStates()
+    newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+
+    # first, calculate how much food on board -- the less the better
+    tiles = 0
+    amt_food = 0
+    for food_row in newFood:
+        for food in food_row:
+            if food:
+                amt_food = amt_food + 1
+            tiles = tiles + 1
+
+    # distance to food
+    food_list = newFood.asList()
+    near_food = tiles*tiles
+    for food_c in food_list:
+        dist = (abs(newPos[0] - food_c[0])**2 + abs(newPos[1] - food_c[1])**2)**(1/2)
+        if dist < near_food:
+            near_food = dist
+
+
+    # calculate Manhattan distance of all ghosts to pacman -- the larger the number, the better
+    total_dist = []
+    for g in newGhostStates:
+        g_pos = g.getPosition()
+        dist = (abs(newPos[0] - g_pos[0])**2 + abs(newPos[1] - g_pos[1])**2)**(1/2)
+        total_dist.append(dist)
+
+
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # i played around with the factors until i got all the point -- i have no idea about the particular numbers/factors that were best for Jerry's performance
+    score = successorGameState.getScore() - amt_food/tiles - 5/( min(total_dist) + 1) + 12/(near_food)
+    return score
+
 
 # Abbreviation
 better = betterEvaluationFunction
